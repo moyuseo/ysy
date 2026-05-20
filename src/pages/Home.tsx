38c125d0bae863206c24b3168bb5efd875dafd75
moyuseo <moyuseo@users.noreply.github.com>
2026-05-20 09:51:54 +0000

    feat: 中药材行业信息网站规划
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { herbs, priceList, newsList, marketIndexes, suppliers } from '../data/mockData';
+import { herbs, priceList, marketIndexes } from '../data/mockData';
 
 const Home: React.FC = () => {
   return (
